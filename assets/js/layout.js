0653b12a1d7f3101547cb1ec4640c54e69781324
Исупов Евгений Валерьевич <workline521@gmail.com>
2024-01-30 13:20:16 +0400

    sound

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -63,30 +63,11 @@ let x = setInterval( function() {
     // if the countdown is over - do smth - write text
     if(distance < 0){
         clearInterval(x);
-        endDate = "06/01/2024 00:00:00";
         let body = document.querySelector("body");
         body.classList.remove("on");
-        //document.getElementById('time').style.display = 'none';
-        //document.querySelector('.newYear').style.display = 'block';
+        const audio = document.querySelector("audio");
+        if (!audio) return;
+        audio.currentTime = 0;
+        audio.play();
     }
 })
-
-
-/* function removeTransition(e) {
-  if (e.propertyName !== "transform") return;
-  e.target.classList.remove("playing");
-}
-
-function playSound(e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-  if (!audio) return;
-
-  key.classList.add("playing");
-  audio.currentTime = 0;
-  audio.play();
-}
-
-const keys = Array.from(document.querySelectorAll(".key"));
-keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
-window.addEventListener("keydown", playSound); */
