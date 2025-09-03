61678ee18e6bb52996ffc0ebc386b28abfbcf778
Исупов Евгений Валерьевич <workline521@gmail.com>
2025-09-03 14:05:55 +0400

    new landing

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -1,26 +1,3 @@
-/* mobile menu */
+document.addEventListener("DOMContentLoaded", function () {
 
-const body = document.querySelector('body');
-const burger = document.querySelector('.header__burger');
-const modalBtns = document.querySelectorAll(".modal");
-const modal = document.querySelector(".popup");
-const closeBtn = document.querySelector(".popup__close");
-
-burger.addEventListener('click', () => {
-	body.classList.toggle('show');
-	burger.classList.toggle('show');
-})
-
-closeBtn.addEventListener("click", () => {
-	modal.classList.remove("active");
-	body.classList.remove("active");
 });
-
-if (modalBtns) {
-	modalBtns.forEach((modalBtn) => {
-		modalBtn.addEventListener("click", function () {
-			modal.classList.add("active");
-			body.classList.add("active");			
-		});
-	});
-}   
